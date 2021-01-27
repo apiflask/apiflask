a788286365e71c1eb2b4b85664d30d0a2d109e90
Grey Li <withlihui@gmail.com>
2021-01-27 16:45:23 +0800

    Work on 0.2.0

--- apiflask/__init__.py
+++ apiflask/__init__.py
@@ -2,4 +2,4 @@ from .core import APIFlask  # noqa: F401
 from .decorators import authenticate, arguments, body, response, \
     other_responses  # noqa: F401
 
-__version__ = '0.1.0'
+__version__ = '0.2.0dev'
