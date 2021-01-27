456967d8d1f88b0e72d3a886521a73938a3ceb2b
Grey Li <withlihui@gmail.com>
2021-01-27 13:41:57 +0800

    Reset package version

--- flask_apitools/__init__.py
+++ flask_apitools/__init__.py
@@ -2,4 +2,4 @@ from .core import APITools  # noqa: F401
 from .decorators import authenticate, arguments, body, response, \
     other_responses  # noqa: F401
 
-__version__ = '0.2.0dev'
+__version__ = '0.1.0dev'
