de5bc30d90ceb2ba83a172054bdfae7276381ca5
Grey Li <withlihui@gmail.com>
2021-03-14 22:06:13 +0800

    Revert "Fix query parameter generation"
    This reverts commit 36d9a48a7d69c4596ab7d3d35cf7ecb6ab0727e6.

--- apiflask/openapi.py
+++ apiflask/openapi.py
@@ -365,7 +365,7 @@ class _OpenAPIMixin:
                     'parameters': [
                         {'in': location, 'schema': schema}
                         for schema, location in view_func._spec.get('args', [])
-                        if location == 'query'
+                        if location != 'body'
                     ],
                     'responses': {},
                 }
