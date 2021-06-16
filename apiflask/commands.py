2a08c17da4f27144b3be1fcbb7b81df39253ce68
Grey Li <withlihui@gmail.com>
2021-06-16 20:56:38 +0800

    Add flask spec command
    Add new configs: SPEC_FORMAT, LOCAL_SPEC_PATH, and LOCAL_SPEC_JSON_INDENT

---
+++ apiflask/commands.py
@@ -0,0 +1,44 @@
+import json
+
+import click
+from flask import current_app
+from flask.cli import with_appcontext
+
+
+@click.command('spec')
+@click.option(
+    '--format',
+    '-f',
+    type=click.Choice(['json', 'yaml', 'yml']),
+    help='The format of the spec, defaults to SPEC_FORMAT config.'
+)
+@click.option(
+    '--output',
+    '-o',
+    type=click.Path(),
+    help='The file path to the spec file, defauts to LOCAL_SPEC_PATH config.'
+)
+@click.option(
+    '--indent',
+    '-i',
+    type=int,
+    help='The indent for JSON spec, defauts to LOCAL_SPEC_JSON_INDENT config.'
+)
+@with_appcontext
+def spec_command(format, output, indent):
+    """Output the OpenAPI spec to stdout or a file."""
+    spec_format = format or current_app.config['SPEC_FORMAT']
+    spec = current_app.get_spec(spec_format)
+    output_path = output or current_app.config['LOCAL_SPEC_PATH']
+    json_indent = indent or current_app.config['LOCAL_SPEC_JSON_INDENT']
+
+    if spec_format == 'json':
+        spec = json.dumps(spec, indent=json_indent)
+
+    # output to stdout
+    click.echo(spec)
+
+    # output to local file
+    if output_path:
+        with open(output_path, 'w') as f:
+            click.echo(spec, file=f)
