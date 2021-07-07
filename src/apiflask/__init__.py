899ba6898287af9a17285074fd0f5813ca3b1219
Grey Li <withlihui@gmail.com>
2021-07-07 13:12:30 +0800

    Release 0.8.0

--- src/apiflask/__init__.py
+++ src/apiflask/__init__.py
@@ -23,4 +23,4 @@ from .schemas import Schema as Schema
 from .security import HTTPBasicAuth as HTTPBasicAuth
 from .security import HTTPTokenAuth as HTTPTokenAuth
 
-__version__ = '0.7.1dev'
+__version__ = '0.8.0'
