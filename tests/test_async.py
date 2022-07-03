3dafa728ada941cf7ee1c6e63666ace78b509022
9gl <glunkad007@gmail.com>
2022-07-03 16:05:37 +0530

    Changed pytest autouse fixture in /tests/test_async.py

--- tests/test_async.py
+++ tests/test_async.py
@@ -4,14 +4,15 @@ from openapi_spec_validator import validate_spec
 from .schemas import FooSchema
 from apiflask import HTTPTokenAuth
 
-
-def skip_flask1(app):
+@pytest.fixture(autouse=True)
+def skip_async_test(app):
     if not hasattr(app, 'ensure_sync'):
         pytest.skip('This test requires Flask 2.0 or higher')
 
 
+
 def test_async_view(app, client):
-    skip_flask1(app)
+    skip_async_test(app)
 
     @app.get('/')
     async def index():
@@ -23,7 +24,7 @@ def test_async_view(app, client):
 
 
 def test_async_error_processor(app, client):
-    skip_flask1(app)
+    skip_async_test(app)
 
     @app.error_processor
     async def custom_error_processor(e):
@@ -35,7 +36,7 @@ def test_async_error_processor(app, client):
 
 
 def test_async_spec_processor(app, client):
-    skip_flask1(app)
+    skip_async_test(app)
 
     @app.spec_processor
     async def update_spec(spec):
@@ -49,7 +50,7 @@ def test_async_spec_processor(app, client):
 
 
 def test_auth_required_on_async_view(app, client):
-    skip_flask1(app)
+    skip_async_test(app)
     auth = HTTPTokenAuth()
 
     @app.get('/')
@@ -62,7 +63,7 @@ def test_auth_required_on_async_view(app, client):
 
 
 def test_doc_on_async_view(app, client):
-    skip_flask1(app)
+    skip_async_test(app)
 
     @app.get('/')
     @app.doc(summary='Test Root Endpoint')
@@ -75,7 +76,7 @@ def test_doc_on_async_view(app, client):
 
 
 def test_input_on_async_view(app, client):
-    skip_flask1(app)
+    skip_async_test(app)
 
     @app.post('/')
     @app.input(FooSchema)
@@ -89,7 +90,7 @@ def test_input_on_async_view(app, client):
 
 
 def test_output_on_async_view(app, client):
-    skip_flask1(app)
+    skip_async_test(app)
 
     @app.get('/foo')
     @app.output(FooSchema)
@@ -110,7 +111,7 @@ def test_output_on_async_view(app, client):
 
 
 def test_async_doc_input_and_output_decorator(app, client):
-    skip_flask1(app)
+    skip_async_test(app)
 
     @app.post('/')
     @app.doc(summary='Test Root Endpoint')
