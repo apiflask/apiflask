d8f84dae5a8a859abbf4aa4213343e2f3b15eb57
180909 <734461790@qq.com>
2024-04-21 17:06:41 +0800

    Switch to pyproject.toml and deprecate the `__version__` attr (#560)
    * Switch to pyproject.toml
    * Improve the implementation and wording
    * Add `build` package and update dependencies
    * Deprecate the `__version__` attribute.
    
    ---------
    
    Co-authored-by: Grey Li <withlihui@gmail.com>

--- src/apiflask/__init__.py
+++ src/apiflask/__init__.py
@@ -1,3 +1,5 @@
+import typing as t
+
 from . import fields as fields
 from . import validators as validators
 from .app import APIFlask as APIFlask
@@ -13,4 +15,19 @@ from .schemas import Schema as Schema
 from .security import HTTPBasicAuth as HTTPBasicAuth
 from .security import HTTPTokenAuth as HTTPTokenAuth
 
-__version__ = '2.1.2-dev'
+
+def __getattr__(name: str) -> t.Any:  # pragma: no cover
+    if name == '__version__':
+        import importlib.metadata
+        import warnings
+
+        warnings.warn(
+            "The '__version__' attribute is deprecated and will be removed in"
+            ' APIFlask 3.0.0. Use feature detection or'
+            " 'importlib.metadata.version(\"apiflask\")' instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return importlib.metadata.version('apiflask')
+
+    raise AttributeError(name)
