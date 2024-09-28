50d7d9a4ce467b4802af0aa0b352d8e9eb58c3a7
Achyut Paudel <70857662+AchPaudel@users.noreply.github.com>
2024-09-28 13:06:26 -0400

    Update app.py

--- examples/basic/app.py
+++ examples/basic/app.py
@@ -24,34 +24,3 @@ errorLogs = []
 def get_hello_world():
     app.logger.info('Entering get /api/hello')
     return jsonify("Hello world")
-
-@app.route('/logs', methods=['GET'])
-def getLogs():
-    app.logger.info('Entering get api/logs')
-    f = open('/home/achyut_p/pythonscripts/logs/gunicorn.access.log', 'r')
-    for x in f:
-        retrieveData = x.replace('\n', '').replace('\"\"', '')
-        parseLog = retrieveData.replace('-', '').replace('\"-\"', '')
-        splitSmart = shlex.split(parseLog)
-        hostAddr = splitSmart[0]
-        date = splitSmart[1].replace('[', '')
-        req = splitSmart[3]
-        returnCode = splitSmart[4]
-        logItem = {
-            'hostname':hostAddr,
-            'date':date,
-            'requestDetails':req.replace(' HTTP/1.1', ''),
-            'responseCode':returnCode,
-            'executionTime':0
-            }
-        accessLogs.append(logItem)
-    f.close()
-
-    e = open('/home/achyut_p/pythonscripts/logs/gunicorn.error.log', 'r')
-
-    for x in e:
-        errorLogs.append(x)
-    e.close()
-
-    jsonLogsStruct = {'accessLogs':accessLogs, 'errorLogs':errorLogs}
-    return jsonify(jsonLogsStruct)
